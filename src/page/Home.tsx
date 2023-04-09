a9dab40d9723b33e0ce0f5ac7ba72ced24ba16f8
Aditya Kumar <aditya@sigaramtech.com>
2023-04-09 22:09:36 +0530

    add message event

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -5,8 +5,10 @@ import { Socket } from "socket.io-client"
 function Home() {
   const socket = useContext<Socket>(SocketContext) 
   const [isConnected, setIsConnected] = useState(socket.connected)
-  const [ messages, setMessages ] = useState<Map<string, string[]>>(new Map());
   const [peers, setPeers] = useState<string[]>([])
+  const [ messages, setMessages ] = useState<Map<string, string[]>>(new Map());
+  const [ data, setData ] = useState('')
+  const [ activePeer, setActivePeer ] = useState<string | null>(null)
 
   useEffect(() => {
     socket.connect();
@@ -20,26 +22,48 @@ function Home() {
     }
 
     function onRecivePeers(peers: string) {
-      setPeers(JSON.parse(peers))
+      const peers_data = JSON.parse(peers)
+      setPeers((peers_data as string[]).filter(s => s !== socket.id))
+    }
+
+    function onMessage(payload: { from: string, data: string }) {
+      console.log('recived message', JSON.stringify(payload));
     }
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     socket.on('peers', onRecivePeers)
+    socket.on('msg', onMessage)
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.off('peers', onRecivePeers)
+      socket.off('msg', onMessage)
     };
   }, []);
 
   return (
     <div>
       {isConnected ? 'Connected!' : 'Not Connected!'} 
-      {peers.map(peer => (
-        <p>{peer}</p>
-      ))}
+        {peers.map(peer => (
+          <button 
+            key={peer} 
+            className={`${activePeer === peer ? 'text-red-500' :'text-green-500'} block`}
+            onClick={() => setActivePeer(peer)}
+          >{peer}</button>
+        ))}
+        {activePeer && (
+          <>
+            <input type="text" className="border-2 border-red-500" onChange={(e) => setData(e.target.value)}></input>
+            <button 
+              className="border-2 px-3" 
+              onClick={() => {
+                socket.emit('msg', { to: activePeer, data })
+              }}
+            >Send</button>
+          </>
+        )}
     </div>
   )
 }
